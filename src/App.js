25713193cd927d27b6cab88d9d35e1ae00bb2951
박민정 <ming1320@gmail.com>
2023-08-29 09:33:21 +0900

    20230828 수업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Content from './companents/Content';
+import Nav from './companents/Nav';
+
+// function Content(props){
+//   console.log(props)
+//   let Title = ["쌍둥이 아기판다", "쌍둥이 아기판다", "주토피아 동물 아카데미", "귀엽게 찰칵!"]
+//   let Desc = ["스페셜 이벤트", "이름을 지어주세요", "판다는 곰이 맞을까?", "에버랜드 AR필터"]
+//   return(
+//   <div className='content-item'>
+//     <img src="https://via.placeholder.com/320" alt="테스트" />
+//     <h3>{Title[props.arrayNum]}</h3>
+//     <p>{Desc[props.arrayNum]}</p>
+//     <p>{props.desc2}</p>
+//   </div>
+//   )
+// }
+
 
 function App() {
+
+
+  
+  // 앞에는 값이 들어가고 뒤에는 함수 > 함수로 해당 값 변경 가능
+  // let Name = "박민정";
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          파일 변경됨...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      {/* <h3 className={Name} style={{color: "orange"}}>Hello React {Name}</h3> */}
+      <div className='content'>
+        <Content arrayNum = "0" />
+        <Content arrayNum = "1" />
+        <Content arrayNum = "2" />
+        <Content arrayNum = "3" />
+      </div>
+      <div className='content'>
+        <Content title="쌍둥이 아기판다" desc="스페셜 이벤트" desc2="테스트1" arrayNum = "0" />
+        <Content title="쌍둥이 아기판다" desc="이름을 지어주세요" desc2="테스트2" arrayNum = "1" />
+        <Content title="주토피아 동물 아카데미" desc="판다는 곰이 맞을까?" desc2="테스트3" arrayNum = "2" />
+        <Content title="귀엽게 찰칵!" desc="에버랜드 AR필터" desc2="테스트4" arrayNum = "3" />
+      </div>
     </div>
   );
 }
---
+++ src/companents/Content.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+function Content(props) {
+//   console.log(props)  
+  let [good, setGood] = useState(0)
+
+  let [title, setTitle] = useState(["쌍둥이 아기판다", "쌍둥이 아기판다", "주토피아 동물 아카데미", "귀엽게 찰칵!"]);
+  let [desc, setDesc] = useState(["스페셜 이벤트", "이름을 지어주세요", "판다는 곰이 맞을까?", "에버랜드 AR필터"])
+
+  function goodFun(){
+    setGood(good+1)
+    // 원본 값인 good에서 값을 1씩 더함
+  }
+  function Change(){
+    let copy = [...title];
+    // 원본 배열 그대로 두고 복사본 사용
+    copy[0] = "쌍둥이 아기판다20"
+    setTitle(copy)
+    let copy2 = [...desc]
+    copy2[0] = "스페셜 20"
+    setDesc(copy2)
+  }
+  let [modalChk, setModalChk] = useState(false)
+    //앞의 값은 비교하는 것 , 바꿔야 되는 값은 뒤에값   
+  function Modal(){
+    // if문으로 먼저 해보기 > 삼항연산자로 바꾸기
+    // setModalChk(true)
+    // (modalChk === true ? setModalChk(false) : setModalChk(true))
+    setModalChk(!modalChk)
+    // modalChk 가 true 일 때는 false, false일 때는 true로 변경
+    // 반대되는 값 변경
+  }
+  return (
+    <>
+        <div className='content-item' onClick={Modal}>
+            <img src="https://via.placeholder.com/320" alt="테스트" />
+            <h3>{title[props.arrayNum]}</h3>
+            <p>{desc[props.arrayNum]}</p>
+            <p onClick={goodFun}>👍 {good}</p>
+            <button onClick={Change}>수정</button>
+        </div>
+        {
+            (modalChk === true && <div className='window' onClick={Modal}></div>)
+        }
+    </>
+  )
+}
+
+export default Content
